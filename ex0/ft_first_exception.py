09093690e8c5d8120adc50bb112988d763aebab1
Ruthler Louis <141589978+Haitianboy94@users.noreply.github.com>
2025-12-26 21:18:30 +0100

    Refactor temperature check and add test function
    Refactor temperature checking logic to return messages instead of printing. Added a test function to validate temperature inputs.

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,18 +1,32 @@
 def check_temperature(temp_str):
 
     try:
-        tmp = int(temp_str)
-        if tmp >= 0 and tmp <= 40:
-            print(f"Temperature {tmp}°C is perfect for plants!\n")
-        elif tmp > 40:
-            print(f"Error: {tmp}°C is too hot for plants (max 40°C)\n")
-        else:
-            print(f"Error: {tmp}°C is too cold for plants (min 0°C)\n")
+        new_tmp = int(temp_str)
+        if new_tmp >= 0 and new_tmp <= 40:
+            return f"Temperature {new_tmp}°C is perfect for plants"
+        elif new_tmp < 0:
+            return f"Error: {new_tmp}°C is too cold for plants (min 0°C)"
+        elif new_tmp > 40:
+            return f"Error: {new_tmp}°C is too hot for plants (max 40°C)"
     except ValueError:
-        print(f"Error: '{temp_str}' is not a valid number\n")
+        return f"{temp_str} is not a valid number"
+
+
+def test_temperature_input():
+    check_with_good = 25
+    print(f"Testing temperature: {check_with_good}\n"
+          f"{check_temperature(check_with_good)}\n")
+    check_with_bad = "abc"
+    print(f"Testing temperature: {check_with_bad}\n"
+          f"{check_temperature(check_with_bad)}\n")
+    check_extrem_posi = 100
+    print(f"Testing temperature: {check_extrem_posi}\n"
+          f"{check_temperature(check_extrem_posi)}\n")
+    check_extrem_nega = -50
+    print(f"Testing temperature: {check_extrem_nega}\n"
+          f"{check_temperature(check_extrem_nega)}\n")
     print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
-    user = input("Testing temperature:")
-    check_temperature(user)
+    test_temperature_input()
